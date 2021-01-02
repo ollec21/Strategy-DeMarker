8d4606eb5ae7eb13475630a33cc21b7b030fff84
kenorb <kenorb@users.noreply.github.com>
2021-01-02 20:58:46 +0000

    Optimize SignalOpenMethod (pf=0.00; dd=0.03; tt=2)

--- config/EURUSD_M30.h
+++ config/EURUSD_M30.h
@@ -16,7 +16,7 @@ struct Stg_DeMarker_Params_M30 : StgParams {
   // Struct constructor.
   Stg_DeMarker_Params_M30() : StgParams(stg_demarker_defaults) {
     lot_size = 0;
-    signal_open_method = 0;
+    signal_open_method = 4;
     signal_open_filter = 1;
     signal_open_level = 0;
     signal_open_boost = 0;
