78186be25418184c9e70d67a4ee9d29b8f8f1f4b
kenorb <kenorb@users.noreply.github.com>
2021-01-04 22:17:24 +0000

    GHA: Sets BtMonths to 10-12

--- config/EURUSD_M15.h
+++ config/EURUSD_M15.h
@@ -18,10 +18,10 @@ struct Stg_DeMarker_Params_M15 : StgParams {
     lot_size = 0;
     signal_open_method = 0;
     signal_open_filter = 1;
-    signal_open_level = 0.2ff;
+    signal_open_level = (float)0.2;
     signal_open_boost = 0;
     signal_close_method = 0;
-    signal_close_level = 0f;
+    signal_close_level = (float)0;
     price_stop_method = 0;
     price_stop_level = 1;
     tick_filter_method = 1;
