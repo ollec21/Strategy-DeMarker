bb6787584b035bbd0e3145c7cc28e7f394e033ca
kenorb <kenorb@users.noreply.github.com>
2021-01-05 23:02:47 +0000

    Optimize PriceStop params (pf=0.63; dd=5.75; tt=7144)

--- config/EURUSD_M5.h
+++ config/EURUSD_M5.h
@@ -22,7 +22,7 @@ struct Stg_DeMarker_Params_M5 : StgParams {
     signal_open_boost = 1;
     signal_close_method = 0;
     signal_close_level = (float)0.0;
-    price_stop_method = 0;
+    price_stop_method = 1;
     price_stop_level = 0.0;
     tick_filter_method = 1;
     max_spread = 0;
